41b64e340c65ca8ac1a9423218ab21f054c2e207
Sneha-Satish <sneha.satish@valtech.com>
2026-07-22 13:09:20 +0530

    customer-service and account-service integrated with eureka partially

--- customer-service/src/main/java/com/bank/customer_service/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/bank/customer_service/CustomerServiceApplication.java
@@ -1,7 +1,9 @@
 package com.bank.customer_service;
 
+import com.netflix.discovery.EurekaNamespace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class CustomerServiceApplication {
