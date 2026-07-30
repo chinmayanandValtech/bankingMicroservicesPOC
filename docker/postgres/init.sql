05478325e2daffb7a2037a2d71041a260d3d2c9f
chinmayanand-g_valtech <chinmayanand.g@valtech.com>
2026-07-30 13:17:10 +0530

    Implemented JWT authorization

--- docker/postgres/init.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE account_db;
-
-CREATE DATABASE customer_db;
-
-CREATE DATABASE transaction_db;
---
+++ infrastructure/docker/postgres/init.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE account_db;
+
+CREATE DATABASE customer_db;
+
+CREATE DATABASE transaction_db;
+
+CREATE DATABASE auth_db;
