c365af3ee9a7d309a35224ece76132d84de8500d
chinmayanand-g_valtech <chinmayanand.g@valtech.com>
2026-08-06 19:33:18 +0530

    All services are running on kubernities now

---
+++ services/account-service/src/main/java/com/bank/account_service/controller/DebugController.java
@@ -0,0 +1,19 @@
+package com.bank.account_service.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/debug")
+public class DebugController {
+
+    @Value("${HOSTNAME:unknown}")
+    private String hostname;
+
+    @GetMapping("/pod")
+    public String pod() {
+        return hostname;
+    }
+}
